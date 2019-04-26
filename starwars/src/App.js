c1097ccfeff0137cee8cf3980fc1729da7c647dd
Jonathan Taylor <jonyonson@icloud.com>
2019-04-26 11:41:21 -0500

    base styles

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      starwarsChars: []
+      starwarsChars: [],
     };
   }
 
@@ -32,7 +32,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="App__Header">React Wars</h1>
       </div>
     );
   }
